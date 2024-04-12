d9b83a2acbf8cdb538e8d3b06b9c40d6e6366486
OmarMWarraich <earthling986@gmail.com>
2024-04-12 23:51:36 +0500

    feat: Client React Displays Posts

---
+++ client/src/features/PostsList.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { API_URL } from "../constants";
+
+const PostsList = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    console.log(API_URL);
+    async function fetchPosts() {
+      try {
+        const response = await fetch(API_URL);
+        console.log(response);
+        if (response.ok) {
+          const data = await response.json();
+          setPosts(data);
+        } else {
+          throw new Error("Failed to fetch posts");
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default PostsList;
