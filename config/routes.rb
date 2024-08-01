b3aaa4c15becd805d739d74ebe509d35c065beb5
OmarMWarraich <earthling986@gmail.com>
2024-08-01 16:37:58 +0500

    feat: Devise Controllers, Jwt

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   # API routes should be versioned to avoid breaking changes.
   namespace :api do
-    namespace :v1 do
+    namespace :v3 do
+      devise_for :users, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'api/v3/users/sessions',
+        registrations: 'api/v3/users/registrations'
+      }
+
       get 'search/posts'
       resources :posts
     end
